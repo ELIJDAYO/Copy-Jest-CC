53dc591ef3b4304694158067c9bdc8dba85d2ed0
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-09-17 21:22:52 +0800

    Trying out different matchers method

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,46 @@
-const sum = require("./sum")
+const sum = require('./sum');
 
-it("should add 1 + 2 to equal to 3", () =>{
+describe('example tests', () => {
+  it('should add 1 + 2 to equal to 3', () => {
     const result = sum(1, 2);
-    expect(result).toBe(3)
+    expect(result).toBe(3);
+  });
+
+  it('object assignment', () => {
+    const obj = {};
+    expect(obj).toEqual({});
+  });
+});
+
+describe('truthy or falsey', () => {
+  it('null', () => {
+    const n = null;
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeNull();
+    expect(n).not.toBeUndefined();
+  });
+});
+
+describe('arrays', () => {
+  const shoppingLists = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  expect(shoppingLists).toContain('milk');
+});
+
+function compileAndroidCode(){
+    throw new Error("you are using the wrong JDK")
+}
+
+describe("exceptions", () => {
+    it("compiling android goes as expected", () =>{
+        expect(() => compileAndroidCode()).toThrow(
+            "you are using the wrong JDK"
+        )
+    })
 })
